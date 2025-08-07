161d3c2e5bdfbfc152f320d5c5cb8961b8c3dcb2
Mustafa Evleksiz <75567558+mystafe@users.noreply.github.com>
2025-08-08 00:34:20 +0400

    feat: make auth optional (#35)

--- src/lib/authOptions.ts
+++ src/lib/authOptions.ts
@@ -2,15 +2,20 @@ import { type NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import AppleProvider from 'next-auth/providers/apple';
 
+const providers =
+  process.env.FEATURE_AUTH === 'true'
+    ? [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID!,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+        AppleProvider({
+          clientId: process.env.APPLE_CLIENT_ID!,
+          clientSecret: process.env.APPLE_CLIENT_SECRET!,
+        }),
+      ]
+    : [];
+
 export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-    AppleProvider({
-      clientId: process.env.APPLE_CLIENT_ID!,
-      clientSecret: process.env.APPLE_CLIENT_SECRET!,
-    }),
-  ],
+  providers,
 };
