0f332b1d39ca4af733d3c3c4dc77f1126db9ce4d
Mustafa Evleksiz <75567558+mystafe@users.noreply.github.com>
2025-08-08 19:34:34 +0400

    fix career page prerender (#49)

--- src/app/career/page.tsx
+++ src/app/career/page.tsx
@@ -1,10 +1,5 @@
-import { motion } from 'framer-motion';
 import CareerPage from '@/components/CareerPage';
 
 export default function Page() {
-  return (
-    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-      <CareerPage />
-    </motion.div>
-  );
+  return <CareerPage />;
 }
