b114a76e8a6e731e84b836fbaf516d4e8ddda09a
Mustafa Evleksiz <75567558+mystafe@users.noreply.github.com>
2025-08-08 23:33:51 +0400

    chore: remove career stubs

--- src/app/career/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react';
-
-export default function Loading() {
-  return (
-    <div className="flex justify-center py-8">
-      <Loader2 className="h-6 w-6 animate-spin" />
-    </div>
-  );
-}
--- src/app/career/page.tsx
+++
@@ -1,5 +0,0 @@
-import CareerPage from '@/components/CareerPage';
-
-export default function Page() {
-  return <CareerPage />;
-}
